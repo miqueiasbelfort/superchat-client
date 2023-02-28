49f20ec315a761676fc57501905a20cdd534a926
miqueiasbelfort <kawasousa1554@gmail.com>
2023-02-28 10:59:27 -0300

    links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,9 +8,9 @@ const Header: React.FC = () => {
     <div className={Styles.container}>
         <h1><span className={Styles.logo}><span id={Styles.logo_}>Super</span>Chat</span></h1>
         <div className={Styles.links}>
-            <a href="#"><FaGithubSquare/></a>
-            <a href="#"><FaInstagramSquare/></a>
-            <a href="#"><FaLink/></a>
+            <a href="https://github.com/miqueiasbelfort"><FaGithubSquare/></a>
+            <a href="https://www.instagram.com/miqueiasbelfort/"><FaInstagramSquare/></a>
+            <a href="https://miqueiasbelfort.netlify.app/"><FaLink/></a>
         </div>
     </div>
   );
